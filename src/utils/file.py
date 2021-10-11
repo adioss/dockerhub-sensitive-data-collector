a24cbe80108ad69f47ad8ab2a039d28961b2a118
adioss <adrien.pailhes@outlook.com>
2021-10-11 18:25:24 +0200

    Bug: update content related to pylint

--- src/utils/file.py
+++ src/utils/file.py
@@ -6,6 +6,6 @@ from utils.config import Config
 
 def write_result(sensitive_data: Result):
     """ Write results to output path """
-    logging.info(f"{sensitive_data.repository}:{sensitive_data.tag} : {sensitive_data.to_json()}")
+    logging.info("%s:%s : %s", sensitive_data.repository, sensitive_data.tag, sensitive_data.to_json())
     with open(Config.get_instance().output_path(), "a", encoding="utf-8") as output_file:
         output_file.write(f"{sensitive_data.to_json()}\n")
