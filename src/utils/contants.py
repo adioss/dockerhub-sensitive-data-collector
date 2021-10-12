59aefdeecf70cc087ee47c52cd120358f51f632b
adioss <adrien.pailhes@outlook.com>
2021-10-12 09:23:18 +0200

    Bug: typo in search url

--- src/utils/contants.py
+++ src/utils/contants.py
@@ -1,3 +1,3 @@
 DOCKERHUB_URL = "https://hub.docker.com"
-DOCKERHUB_SEARCH_URL = f"{DOCKERHUB_URL}%s/api/content/v1/products/search"
+DOCKERHUB_SEARCH_URL = f"{DOCKERHUB_URL}/api/content/v1/products/search"
 SEARCH_PAGE_SIZE = "10"
